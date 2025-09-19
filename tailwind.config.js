382f1b2476e180f64e26a03858436be1ec7a053d
geoffrey <geoffreyllk@gmail.com>
2025-09-19 22:16:01 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './*.html',
+    './**/*.html',
+    './*.js'
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: []
+}
